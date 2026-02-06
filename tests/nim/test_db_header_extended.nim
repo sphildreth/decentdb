8430294aa30210c47a6e6ae32c65648f814d423b
Steven Hildreth <steven@hixi.org>
2026-02-05 22:15:02 -0600

    fix: update FormatVersion to 1 for compatibility

--- tests/nim/test_db_header_extended.nim
+++ tests/nim/test_db_header_extended.nim
@@ -185,7 +185,7 @@ suite "DB Header Extended":
     check HeaderSize == 128
     check MagicBytes == "DECENTDB"
     check MagicPadded.len == 16
-    check FormatVersion == 8'u32
+    check FormatVersion == 1'u32
     check DefaultPageSize == 4096'u32
 
   test "readHeader from empty file fails":
