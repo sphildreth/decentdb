4b88368319b3409cf2e929b8d99fe15890d0dc16
Steven Hildreth <steven@hixi.org>
2026-02-05 21:56:07 -0600

    fix: revert FormatVersion to 1 for compatibility

--- src/pager/db_header.nim
+++ src/pager/db_header.nim
@@ -6,7 +6,7 @@ const
   HeaderSize* = 128
   MagicBytes* = "DECENTDB"
   MagicPadded* = MagicBytes & repeat('\0', 16 - MagicBytes.len)
-  FormatVersion* = 8'u32
+  FormatVersion* = 1'u32
   DefaultPageSize* = 4096'u32
 
 type DbHeader* = object
